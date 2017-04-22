b2146c3bd117a74687969845909545ec729d320f
radiohegel <roger.e.clayton@gmail.com>
2017-04-22 15:34:21 -0700

    changes navbar background dark grey

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/create'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  get 'posts/show'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
+
+  resources 'posts'
 end
